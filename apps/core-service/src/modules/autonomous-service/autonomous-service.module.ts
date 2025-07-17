d528e725073233d3b30704e1c89b746932125444
valens200 <valensniyonsenga2003@gmail.com>
2025-07-17 14:23:40 +0200

    feat: creating autonomous services

---
+++ apps/core-service/src/modules/autonomous-service/autonomous-service.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AutonomousServiceService } from './autonomous-service.service';
+import { AutonomousServiceController } from './autonomous-service.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AutonomousService } from './entities/autonomous-service.entity';
+import { MinioClientModule } from '../minio-client/minio-client.module';
+import { SubjectsModule } from '../subjects/subjects.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([AutonomousService]),
+    MinioClientModule,
+    SubjectsModule,
+  ],
+  exports: [AutonomousServiceService],
+  providers: [AutonomousServiceService],
+  controllers: [AutonomousServiceController],
+})
+export class AutonomousServiceModule {}
---
+++ apps/core-service/src/modules/autonomous-service/entities/autonomous-service.entity.ts
@@ -0,0 +1,51 @@
+import { BaseEntity } from '@app/common/database/base.entity';
+import { Subject } from '@core-service/modules/subjects/entities/subject.entity';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { User } from '@core-service/modules/user/entities/user.entity';
+import { EAutonomousServiceStatus } from '../enums/autonomous-service-status.enum';
+import { AttachmentDto } from '@app/common/dtos/attachment.dto';
+
+@Entity()
+export class AutonomousService extends BaseEntity {
+  @Column()
+  projectTitle: string;
+
+  @Column()
+  description: string;
+
+  @Column({
+    type: 'enum',
+    enum: EAutonomousServiceStatus,
+    default: EAutonomousServiceStatus.PENDING,
+  })
+  status: EAutonomousServiceStatus;
+
+  @ManyToOne(() => Subject)
+  @JoinColumn({ name: 'subject_id' })
+  subject: Subject;
+
+  @Column()
+  isOwnerAnonymous: boolean;
+
+  @Column()
+  contractFinalizationDate: Date;
+
+  @Column()
+  finalSubmissionDate: Date;
+
+  @Column({
+    type: 'jsonb',
+    nullable: true,
+  })
+  preferredOutputFormats: string[];
+
+  @Column({
+    type: 'jsonb',
+    nullable: true,
+  })
+  attachments: AttachmentDto[];
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'owner_id' })
+  owner: User;
+}
