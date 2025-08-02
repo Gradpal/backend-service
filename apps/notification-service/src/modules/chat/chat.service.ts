2802c8f1e4eb619ff1040fb9eb8e8110d8b05612
valens200 <valensniyonsenga2003@gmail.com>
2025-08-02 17:28:45 +0200

    feat: create a conversation automatically after session booking

--- apps/notification-service/src/modules/chat/chat.service.ts
+++ apps/notification-service/src/modules/chat/chat.service.ts
@@ -25,12 +25,8 @@ import { ExceptionHandler } from '@app/common/exceptions/exceptions.handler';
 import { MessageOwner } from './dtos/message-owner.dto';
 import { createPaginatedResponse } from '@app/common/helpers/pagination.helper';
 import { CreateConversationRequest } from './dtos/create-conversation.dto';
-<<<<<<< HEAD
 import { UserService } from '@core-service/modules/user/user.service';
 import { NotificationUserService } from '../user/user.service';
-=======
-
->>>>>>> deb8fd1 (fix: fixed getting shared files)
 @Injectable()
 export class ChatService {
   private minioClientService: MinioClientService;
@@ -242,14 +238,6 @@ export class ChatService {
         take: limit,
       });
 
-<<<<<<< HEAD
-=======
-    if (page && limit) {
-      query.skip((page - 1) * limit).take(limit);
-    }
-    const [conversations, total] = await query.getManyAndCount();
-
->>>>>>> deb8fd1 (fix: fixed getting shared files)
     return createPaginatedResponse(conversations, total, page, limit);
   }
 
@@ -417,7 +405,6 @@ export class ChatService {
   }
 
   async createConversation(createConversationDto: CreateConversationRequest) {
-<<<<<<< HEAD
     const existsBySenderAndReceiver =
       await this.getConversationBySenderAndReceiver(
         createConversationDto?.sender?.id,
@@ -445,13 +432,5 @@ export class ChatService {
       status: EConversationStatus.ACTIVE,
     });
     return await this.conversationRepository.save(conversation);
-=======
-    const conversation = this.conversationRepository.create({
-      sender: createConversationDto.sender,
-      receiver: createConversationDto.receiver,
-      status: EConversationStatus.ACTIVE,
-    });
-    return this.conversationRepository.save(conversation);
->>>>>>> deb8fd1 (fix: fixed getting shared files)
   }
 }
